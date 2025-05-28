847da95c7b5c11ac6fb65828c804e8f32d6b7d6b
vitorgilhi <vgilhidasilva@gmail.com>
2025-05-28 11:47:48 -0300

    login page created

--- UI/ProjetoIntegradorAngular/src/app/app.routes.ts
+++ UI/ProjetoIntegradorAngular/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { UserListComponent } from './Admin/user-list/user-list.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { RegisterComponent } from './Pages/register/register.component';
 import { OngTicketFormComponent } from './Pages/ong-ticket-form/ong-ticket-form.component';
+import {LoginComponent} from './Pages/login/login.component';
 
 export const routes: Routes = [
     {path:'Admin-UserList',component: UserListComponent},
     {path:'',component:HomeComponent},
     {path:'Register',component:RegisterComponent},
-    {path:'OngTicketForm',component:OngTicketFormComponent}
+    {path:'OngTicketForm',component:OngTicketFormComponent},
+    {path:'Login',component:LoginComponent}
 ];
