bad4ef2b186942baa2d9ed5f9335c42b70c61ee4
DMenor HGS <vgilhidasilva@gmail.com>
2025-06-24 19:21:26 -0300

    about us page

--- UI/ProjetoIntegradorAngular/src/app/app.routes.ts
+++ UI/ProjetoIntegradorAngular/src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { UserListComponent } from './Admin/user-list/user-list.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { RegisterComponent } from './Pages/register/register.component';
 import { OngTicketFormComponent } from './Pages/ong-ticket-form/ong-ticket-form.component';
-import {LoginComponent} from './Pages/login/login.component';
+import { LoginComponent } from './Pages/login/login.component';
 import { authGuard } from './auth.guard';
+import { AboutUsComponent } from './Pages/about-us/about-us.component';       
 
 export const routes: Routes = [
     {path:'Admin-UserList',component: UserListComponent,canActivate: [authGuard]},
     {path:'',component:HomeComponent},
     {path:'Register',component:RegisterComponent},
     {path:'OngTicketForm',component:OngTicketFormComponent,},
-    {path:'Login',component:LoginComponent}
+    {path:'Login',component:LoginComponent},
+    {path:'AboutUs',component:AboutUsComponent}
 ];
