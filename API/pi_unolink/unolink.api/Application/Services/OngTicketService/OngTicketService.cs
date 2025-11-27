ac4be894f634cc0696b03634d450256325ee315a
MatheusHaruSaito <matheusharusaito@gmail.com>
2025-11-26 22:02:26 -0300

    Strong Password to Ong Tickets

--- API/pi_unolink/unolink.api/Application/Services/OngTicketService/OngTicketService.cs
+++ API/pi_unolink/unolink.api/Application/Services/OngTicketService/OngTicketService.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using unolink.api.Application.Models.Dtos;
+using unolink.api.Application.Models.Request;
 using unolink.api.Application.Models.Request.OngTicket;
+using unolink.api.Application.Services.EmailService;
 using unolink.domain.Core.Interfaces;
 using unolink.domain.Models;
 using unolink.infrastructure.Repositories;
@@ -16,11 +18,13 @@ namespace unolink.api.Application.Services.OngTicketService
         private readonly UserManager<User> _userManager;
         private readonly IUserRepository _userRepository;
         private readonly IOngTicketRepository _ongTicketRepository;
-        public OngTicketService(IOngTicketRepository ongTicketRepository, UserManager<User> userManager, IUserRepository userRepository)
+        private readonly IEmailService _emailService;
+        public OngTicketService(IOngTicketRepository ongTicketRepository, UserManager<User> userManager, IUserRepository userRepository, IEmailService emailService)
         {
             _ongTicketRepository = ongTicketRepository;
             _userManager = userManager;
             _userRepository = userRepository;
+            _emailService = emailService;
         }
 
         public async Task<bool> AcceptTicket(Guid id)
@@ -29,23 +33,31 @@ namespace unolink.api.Application.Services.OngTicketService
             if (ong == null) return false;
             if (ong.Reviwed) return false;
             ong.AcceptedOng();
-            
-            //LEMBRA DE COLOCAR UMA SENHA FORTE ALEATORIA AQUI DEPOIS
-            var user = new User(ong.Name, ong.Email, "", ong.Description, ong.Cep);
+
+            string password = Random.Shared.Next(5125, 99999).ToString();
+            var user = new User(ong.Name, ong.Email, password, ong.Description, ong.Cep);
             _ongTicketRepository.Update(ong);
             var userCreated = await _userManager.CreateAsync(user);
             if (!userCreated.Succeeded)
             {
                 return false;
             }
-            var addedRole =  await _userManager.AddToRoleAsync(user, "Ong");
+            var addedRole = await _userManager.AddToRoleAsync(user, "Ong");
             if (!addedRole.Succeeded)
             {
                 return false;
             }
             await _ongTicketRepository.UnitOfWork.SaveEntitiesAsync();
-            
-            if((await _ongTicketRepository.GetById(id)).Reviwed)
+
+            var email = new CreateEmailRequest()
+            {
+                ToEmail = ong.Email,
+                Subject = "Requisição de Ong Confirmada",
+                Body = $"Sua requisição de conta de ong foi aceita\n Conta: {ong.Email}\n Senha {password}",
+            };
+            await _emailService.EmailSender(email);
+
+            if ((await _ongTicketRepository.GetById(id)).Reviwed)
             {
                 return true;
             }
@@ -75,6 +87,14 @@ namespace unolink.api.Application.Services.OngTicketService
             _ongTicketRepository.Update(ong);
 
             await _ongTicketRepository.UnitOfWork.SaveChangesAsync();
+
+            var email = new CreateEmailRequest()
+            {
+                ToEmail = ong.Email,
+                Subject = "Requisição de Ong Negada",
+                Body = $"Apos analise, podemos concluir que sua requisição é ilegivel para uma conta ong",
+            };
+            await _emailService.EmailSender(email);
 
             return true;
 
